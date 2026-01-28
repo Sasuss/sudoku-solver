d94b1724b2035b038d6b61ffc77268bfc26935d1
Sasus <alexander.klimes@outlook.cz>
2026-01-28 14:49:23 +0100

    finished missing_number, added typing, started woring on implementation

--- basic_solvers.py
+++ basic_solvers.py
@@ -2,24 +2,32 @@ import numpy as np
 from numpy import ndarray
 
 
-def missing_number(arr: ndarray):
-    if len(np.where(arr == None)[0]) > 1:
-        return None
+def missing_number(arr: ndarray) -> ndarray:
+    if len(np.where(arr == None)[0]) != 1:
+        return arr
 
     nums = np.zeros(9)
+    idx = 0
+    final_idx = None
     for a in arr:
         if a is not None:
             np.put(nums, a-1, 1)
+        else:
+            final_idx = idx
+        idx += 1
 
+
+    print(final_idx)
     missing_nums = np.where(nums == 0)
+    print(missing_nums)
 
-    return missing_nums[0][0]+1
+    np.put(arr, final_idx, int(missing_nums[0][0]+1))
+    return arr
 
 
 
-test = np.array([5, 3, 2, 6, 1, 4, None, 9, 8])
+test = np.array([5, 3, 2, 6, 1, 7, None, 9, 8])
 #print(test)
-
 
 print(missing_number(test))
 
--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import numpy as np
 from sudoku_base import extract_blocks, extract_columns, extract_rows
-
+from basic_solvers import missing_number
 
 def get_input():
+
     #sudoku = input()
+    """
     input_sudoku = np.array([
         5, 3, None, None, 7, None, None, None, None,
         6, None, None, 1, 9, 5, None, None, None,
@@ -16,7 +18,18 @@ def get_input():
         None, None, None, None, 8, None, None, 7, 9
     ])
     #side = get_side(sudoku)
-
+    """
+    input_sudoku = np.array([
+        5, 3, 4, 6, 7, 8, 9, 1, None,  # Chybí 2
+        6, 7, 2, 1, 9, 5, 3, 4, None,  # Chybí 8
+        1, 9, 8, 3, 4, 2, 5, 6, None,  # Chybí 7
+        8, 5, 9, 7, 6, 1, 4, 2, None,  # Chybí 3
+        4, 2, 6, 8, 5, 3, 7, 9, None,  # Chybí 1
+        7, 1, 3, 9, 2, 4, 8, 5, None,  # Chybí 6
+        9, 6, 1, 5, 3, 7, 2, 8, None,  # Chybí 4
+        2, 8, 7, 4, 1, 9, 6, 3, None,  # Chybí 5
+        3, 4, 5, 2, 8, 6, 1, 7, None  # Chybí 9
+    ])
     return input_sudoku
 
 
@@ -29,6 +42,8 @@ print(sudoku_rows)
 print(sudoku_columns)
 print(sudoku_blocks)
 
-print(type(sudoku_blocks[0]))
-
-#print(get_input())
+idx = 0
+for a in sudoku_rows:
+    new = missing_number(a)
+    np.put(sudoku_rows, idx, new)
+    idx += 1
